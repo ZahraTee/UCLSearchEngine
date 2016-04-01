412cbbf1db036cec235a0f95d67cdfbcdc889b73
AlSaeed <w.alsaeed@hotmail.com>
2016-04-01 10:47:46 +0100

    Script to do Crawling, Computing PageRank and Indexing

---
+++ init.sh
@@ -0,0 +1,11 @@
+#Crawling
+crawler/scrapy crawl two
+
+#Compute PageRank
+python PageRank.py
+
+#Indexing
+#Construct collection.spec file
+find crawler/doc -type f | grep '.*\.txt' > terrier-core-4.1/etc/collection.spec
+#Construct index
+terrier-core-4.1/bin/trec_terrier.sh -i
